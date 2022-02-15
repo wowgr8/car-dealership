2e7fd1098512743fa5caf53fb75e6b02b37bc598
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-14 19:28:54 -0800

    add controller for Cars
    co-authored-by: Marni Sucher <suchermarni@gmail.com>

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Dealership.Models;
+using System.Collections.Generic;
+
+namespace Dealership.Controllers
+{
+  public class CarsController : Controller
+  {
+
+    [HttpGet("/cars")]
+    public ActionResult Index()
+    {
+      List<Car> allCars = Car.GetAll();
+      return View(allCars);
+    }
+
+    [HttpGet("/cars/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/cars")]
+    public ActionResult Create(string makeModel, int price, int miles)
+    {
+      Car mycar = new Car(makeModel, price, miles);
+      return RedirectToAction("Index");
+    }
+
+  }
+}
